180c816ec8032833627d158c4349ec4b0c31a4b6
Samantha Burke <samanthaburke.uk@gmail.com>
2019-11-23 16:05:19 +0000

    more tidying

--- server.js
+++ server.js
@@ -4,31 +4,12 @@ var app = express();
 var formidable = require('express-formidable');
 // Save blog post data on this machine's hard drive, i.e. file-system
 var fs = require('fs')
-
-
-
-// Converts JSON to string
-
 // Serve the static assets in our public folder
 app.use(express.static("public"));
-
 // Uses the express-formidable library
 app.use(formidable());
 
-// Post to endpoint (html page doesn't exist)
-app.post("/create-post", function(req, res){
-    // Works if sending JSON files
-    // console.log(req.body);
 
-var blogPost = req.fields;
-// Converts JSON to string
-fs.writeFile('data/posts.json', JSON.stringify(blogPost), function (error) {
-    
-});
-
-    // .fields extracts the form data you're receiving
-    //console.log(req.fields);
-});
 
 // __dirname is a Node global object that gives you a path to the working directory
 fs.readFile(__dirname + '/data/posts.json', function (error, file) {
@@ -36,6 +17,20 @@ fs.readFile(__dirname + '/data/posts.json', function (error, file) {
     console.log(file.toString());
     var parsedFile = JSON.parse(file);
 });
+
+// Post to endpoint (html page doesn't exist)
+app.post("/create-post", function(req, res){
+    // Works if sending JSON files
+    var blogPost = req.fields;
+    // Converts JSON to string
+    fs.writeFile('data/posts.json', JSON.stringify(blogPost), function (error) {
+    });
+});
+
+
+
+    // .fields extracts the form data you're receiving
+    //console.log(req.fields);
 
 // Listen for requests on port 3000
 app.listen(3000, function(){
